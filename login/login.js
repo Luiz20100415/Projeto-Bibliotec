113ab1408bfaf17c935d2548eab61125c49e4948
LUIZ GUSTAVO DOS SANTOS E SOUZA <55477595892@info.local>
2026-09-08 17:26:46 -0300

    :construction: Pagína de listagem de livro em andamento

---
+++ login/login.js
@@ -0,0 +1,16 @@
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+console.log(btnSenha.type);
+
+btnSenha.addEventListener("click", function() {
+
+    // if(campoSenha.type == "password") {
+    //     campoSenha.type = "text";
+    // }
+    // else{
+    //     campoSenha.type = "password";
+    // }
+
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password";     
+});
